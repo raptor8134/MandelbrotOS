2537e7703659719489aecff74cc5435ff26d4af7
xhci <76793908+xhci1@users.noreply.github.com>
2022-06-03 05:27:16 +0100

    Update ctype.h

--- src/libc/src/include/ctype.h
+++ src/libc/src/include/ctype.h
@@ -1,6 +1,14 @@
 #ifndef __CTYPE_H__
 #define __CTYPE_H__
 
+int isalnum(int c);
+int isalpha(int c);
+int isblank(int c);
+int iscntrl(int c);
+int isdigit(int c);
+int isupper(int c);
+int islower(int c);
+int isspace(int c);
 int tolower(int c);
 int _tolower(int c);
 int toupper(int c);
