a1b484353753adee8a1fda7f2fe34ceadf081d86
CaydendW <cayden@4dewits.com>
2022-06-15 17:28:39 +0200

    Fixed issue with shell folder and formated code

--- src/libc/src/ctype.c
+++ src/libc/src/ctype.c
@@ -1,31 +1,19 @@
 #include <ctype.h>
 #include <sys/mandelbrot.h>
 
-int isdigit(int c) {
-  return c >= '0' && c <= '9';
-}
+int isdigit(int c) { return c >= '0' && c <= '9'; }
 
 int isspace(int c) {
-  return c == ' '
-    || c == '\f'
-    || c == '\n'
-    || c == '\r'
-    || c == '\t'
-    || c == '\v';
+  return c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' ||
+         c == '\v';
 }
 
-int islower(int c) {
-  return c >= 'a' && c <= 'z';
-}
+int islower(int c) { return c >= 'a' && c <= 'z'; }
 
-int isupper(int c) {
-  return c >= 'A' && c <= 'Z';
-}
+int isupper(int c) { return c >= 'A' && c <= 'Z'; }
 
 int isxdigit(int c) {
-  return isdigit(c)
-    || (c >= 'a' && c <= 'f')
-    || (c >= 'A' && c <= 'F');
+  return isdigit(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
 }
 
 int tolower(int c) {
@@ -44,18 +32,10 @@ int toupper(int c) {
 
 int _toupper(int c) { return toupper(c); }
 
-int isalnum(int c) {
-  return isdigit(c) || isalpha(c);
-}
+int isalnum(int c) { return isdigit(c) || isalpha(c); }
 
-int isalpha(int c) {
-  return isupper(c) || islower(c);
-}
+int isalpha(int c) { return isupper(c) || islower(c); }
 
-int isblank(int c) {
-  return c == ' ' || c == '\t';
-}
+int isblank(int c) { return c == ' ' || c == '\t'; }
 
-int iscntrl(int c) {
-  return (c >= 0 && c < ' ') || c == '\b';
-}
+int iscntrl(int c) { return (c >= 0 && c < ' ') || c == '\b'; }
